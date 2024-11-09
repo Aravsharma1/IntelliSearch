05b14779777d19c77711a31bea719346200b06b7
Aravsharma1 <aravsharma0905@gmail.com>
2024-11-09 17:44:36 -0500

    basic homepage design

--- my-app/app/about-us/page.tsx
+++ my-app/app/about-us/page.tsx
@@ -1,14 +1,11 @@
 // app/page.tsx
 import React from 'react';
-import Link from 'next/link'
 
 export default function HomePage() {
   return (
     <div>
       <h1>Welcome to IntelliSearch!</h1>
-      <p>This is the homepage of the news digest app.</p>
-      <Link href="/about-us"> About Us
-      </Link>
+      <p>This is the about us of the news digest app.</p>
     </div>
   );
 }
