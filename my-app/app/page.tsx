90588c0d9dbe3abbbd696af1023c97e6d24574fa
Aravsharma1 <aravsharma0905@gmail.com>
2024-11-10 03:05:13 -0500

    partially completed homepage design

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -7,8 +7,45 @@ export default function HomePage() {
     <div>
       <h1>Welcome to IntelliSearch!</h1>
       <p>This is the homepage of the news digest app.</p>
-      <Link href="/about-us"> About Us
-      </Link>
+      <Link href="/about-us"> About Us</Link>
+      <MainTextField />
     </div>
   );
+}
+
+
+export function MainTextField() {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+export function DropdownMenu(){
+  return (
+    <div>
+      Dropdown Menu for news genre.
+    </div>
+  )
+}
+
+export function TimeFrame(){
+  return (
+    <div> 
+      Timeframe for the news you want. 
+    </div>
+  )
+}
+
+export function SubmitButton(){
+  return (
+    <button>Submit: click here to get the news you want.</button>
+  )
+}
+
+export function SummarizeContent(){
+  return (
+    <button>Summarize Content: click here to get the news you want summarized using AI.</button>
+  )
 }
