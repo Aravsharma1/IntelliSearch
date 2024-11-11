47c698936e13d129c6c333f4ee1f514f83147b90
Aravsharma1 <aravsharma0905@gmail.com>
2024-11-11 00:33:14 -0500

    finished basic frontend ui for mvp

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,51 +1,71 @@
 // app/page.tsx
 import React from 'react';
-import Link from 'next/link'
+import Link from 'next/link';
+import styles from './HomePage.module.css';
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome to IntelliSearch!</h1>
-      <p>This is the homepage of the news digest app.</p>
-      <Link href="/about-us"> About Us</Link>
+    <div className="container">
+      <h1 className={styles.heading}>Welcome to IntelliSearch!</h1>
+      <p className={styles.intro}>This is the homepage of the news digest app.</p>
+      <div className={styles.buttonDropdownContainer}>
+        <SubmitButton />
+        <DropdownMenu />
+        <TimeFrame />
+      </div>
       <MainTextField />
+      <SummarizeContent />
     </div>
   );
 }
 
-
 export function MainTextField() {
   return (
-    <div>
+    <div className={styles.mainTextField}>
       Hello World
+    </div>
+  );
+}
+
+export function DropdownMenu3() {
+  return (
+    <div className={styles.dropdownMenu}>
+      Dropdown Menu for news genre.
     </div>
   );
 }
 
 export function DropdownMenu(){
   return (
-    <div>
-      Dropdown Menu for news genre.
+    <select name='NewsGenre' id = 'NewsGenre'>
+      <option value="volvo">Technology</option>
+    <option value="saab">Entertainment</option>
+    <option value="mercedes">Geopolitics</option>
+    <option value="audi">All</option>
+    </select>
+  )
+}
+
+export function TimeFrame() {
+  return (
+    <div className={styles.timeFrame}>
+      Timeframe for the news you want.
     </div>
-  )
+  );
 }
 
-export function TimeFrame(){
+export function SubmitButton() {
   return (
-    <div> 
-      Timeframe for the news you want. 
+    <div className={styles.submitButton}>
+      <button>Submit: click here to get the news you want.</button>
     </div>
-  )
+  );
 }
 
-export function SubmitButton(){
+export function SummarizeContent() {
   return (
-    <button>Submit: click here to get the news you want.</button>
-  )
-}
-
-export function SummarizeContent(){
-  return (
-    <button>Summarize Content: click here to get the news you want summarized using AI.</button>
-  )
+    <div className={styles.summarizeContent}>
+      <button>Summarize Content: click here to get the news you want summarized using AI.</button>
+    </div>
+  );
 }
