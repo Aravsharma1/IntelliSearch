f414b139cd2a14882a0ab37282fd424b07e834de
Aravsharma1 <aravsharma0905@gmail.com>
2024-11-28 22:12:28 -0500

    stripe integration done in test mode

--- my-app/app/cancel/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Cancel() {
-    return (
-      <div>
-        <h1>Subscription Canceled</h1>
-        <p>It seems you canceled the subscription process. You can try again anytime.</p>
-      </div>
-    );
-  }
-  
--- my-app/app/success/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Success() {
-    return (
-      <div>
-        <h1>Subscription Successful!</h1>
-        <p>Thank you for subscribing to our service.</p>
-      </div>
-    );
-  }
-  
